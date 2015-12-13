f5e6a75b8aad7cf7b80f17d22247d567f615ded7
Maureen Davey <davey.maureen@gmail.com>
2015-12-13 10:51:54 -0500

    Added option to calculate a target end date based on words per day entered
    Expanded the script to give you the options of calculating your WPD
    needed to meet a deadline, or calculate what your deadline would be if
    you wrote X words per day.  Script now requires use of the
    python-dateutil module.

--- readme.md
+++ readme.md
@@ -2,25 +2,36 @@ Words Per Day Calculator Script
 ---------------------------------------------------------------
 By: Maureen Davey (http://www.maureenldavey.com)
 
-This is a basic Python script meant to calculate how many words you have left to meet your word count goal in a specified amount of time.
+This is a basic Python script meant to calculate word goal counts and target end dates for writing projects.
+
+To calculate your needed words per day to meet a deadline:
 
 Input your start date, end date, total words required, and any words you've done so far.  If you're already past your start date or your start date is today, the script will also ask if you want to include today in your calculations.  It will calculate your remaining words per day and tell you how many days you have left until your deadline.
 
-The script verifies that you did not leave any inputs blank, and that your current total so far and total required are numbers.
+Alternatively, you can enter the number of words per day you're aiming to write, along with your required total, and the script will calculate what your end date will be.
 
-Please re-use/modify this script to meet your needs.  And of course, keep on writing!
+The script verifies that you did not leave any inputs blank, and that numeric variables are in fact numbers.  If you are already partway through a project, you can also enter the number of words you've already written.
+
+This script is open source, please re-use/modify this script to meet your needs.  And of course, keep on writing!
+
+Note: This script was written for Python 2.x.  It may not work with Python 3 and above.
 
 
 Installation Instructions
 ---------------------------------------------------------------
 
-* Confirm Python is installed on your system
+* Confirm Python is installed on your system.  Opening your terminal and entering `python -V` should give you a version number if it is installed
 * Download the script
-* Run the script from the terminal: python wpd-calculator.py
+* Run the script from the terminal:
+```python
+python wpd-calculator.py```
+
+This script also requires the python-dateutil module (http://labix.org/python-dateutil for Python < 3.0, newer versions here https://github.com/dateutil/dateutil) to be installed.  The python-dateutil module was written by Gustavo Niemeyer.
 
 
 To Do
 ---------------------------------------------------------------
-* Confirm dates are inputted in the proper format (i.e. m/d/YYYY)
+* Confirm dates are inputted in the proper format (i.e. m/d/Y)
 * Throw an error if your end date is before your start date or the dates don't logically make sense
 * Be able to calculate additional stats, like an average WPD count to date, or if the end date is already past, if you met your goal and how much you were over/under
+* Test/update for newer versions of Python
--- wpd-calculator.py
+++ wpd-calculator.py
@@ -3,6 +3,8 @@
 # This is a basic Python script meant to calculate how many words you have left to meet your word count goal in a specified amount of time.
 
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 import math
 
 def __format_datetime(string):
@@ -21,60 +23,99 @@ print "---------------------------------------------------------------"
 
 print "Please answer a few questions about your project, and we will calculate your remaining words per day needed to meet your goal."
 
-start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format m/d/yyyy, where the year is four digits.) ")
-while(len(start_date) == 0):
-	print "Sorry, no input was detected.  Please try again."
-	start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format m/d/yyyy, where the year is four digits.) ")
-start_date_full = start_date + " 00:00:00"
+method = raw_input("Would you like to calculate the number of words needed per day to meet a specific end date, or calculate what the end date would be if you wrote X number of words per day?  Enter A for the first option, B for the second. ")
 
-end_date = raw_input("What is the ending date/deadline for your writing project?  This date will be included in the days remaining. (Please answer in the format m/d/yyyy, where the year is four digits.) ")
-while(len(end_date) == 0):
-	print "Sorry, no input was detected.  Please try again."
-	end_date = raw_input("What is the ending date/deadline for your writing project?  This date will be included in the days remaining. (Please answer in the format m/d/yyyy, where the year is four digits.) ")
-end_date_full = end_date + " 23:59:59"
+if method == "A":
+	start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	while(len(start_date) == 0):
+		print "Sorry, no input was detected.  Please try again."
+		start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	start_date_full = start_date + " 00:00:00"
 
-words_total = raw_input("What is the total number of words you need to complete within that timeframe? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
-while(__check_numeric(words_total) == False):
-	print "Sorry, that did not appear to be a number.  Please try again."
+	end_date = raw_input("What is the ending date/deadline for your writing project?  This date will be included in the days remaining. (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	while(len(end_date) == 0):
+		print "Sorry, no input was detected.  Please try again."
+		end_date = raw_input("What is the ending date/deadline for your writing project?  This date will be included in the days remaining. (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	end_date_full = end_date + " 23:59:59"
+
 	words_total = raw_input("What is the total number of words you need to complete within that timeframe? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
+	while(__check_numeric(words_total) == False):
+		print "Sorry, that did not appear to be a number.  Please try again."
+		words_total = raw_input("What is the total number of words you need to complete within that timeframe? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
 
-words_so_far = raw_input("Have you written any words so far that should be included in the total? Just enter the number, no commas.  (If you are not including any words already written, just enter 0). ")
-while(__check_numeric(words_so_far) == False):
-	print "Sorry, that did not appear to be a number.  Please try again."
-	words_total = raw_input("What is the total number of words you need to complete within that timeframe? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
+	words_so_far = raw_input("Have you written any words so far that should be included in the total? Just enter the number, no commas.  (If you are not including any words already written, just enter 0). ")
+	while(__check_numeric(words_so_far) == False):
+		print "Sorry, that did not appear to be a number.  Please try again."
+		words_total = raw_input("What is the total number of words you need to complete within that timeframe? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
 
-now = datetime.now()
-today_date = "%s/%s/%s" % (now.month, now.day, now.year)
-today_full = today_date + " %s:%s:%s" % (now.hour, now.minute, now.second)
+	now = datetime.now()
+	today_date = "%s/%s/%s" % (now.month, now.day, now.year)
+	today_full = today_date + " %s:%s:%s" % (now.hour, now.minute, now.second)
 
-#Find out if the timeline has started yet
-delta_until_start = __format_datetime(start_date_full) - __format_datetime(today_full)
-days_until_start = delta_until_start.days
+	#Find out if the timeline has started yet
+	delta_until_start = __format_datetime(start_date_full) - __format_datetime(today_full)
+	days_until_start = delta_until_start.days
 
-#Find length of the project timeframe
-delta_project_timeframe = __format_datetime(end_date_full) - __format_datetime(start_date_full)
-project_length_days = delta_project_timeframe.days + 1
+	#Find length of the project timeframe
+	delta_project_timeframe = __format_datetime(end_date_full) - __format_datetime(start_date_full)
+	project_length_days = delta_project_timeframe.days + 1
 
-#Calculate info for this user
-words_remaining = int(words_total) - int(words_so_far)
+	#Calculate info for this user
+	words_remaining = int(words_total) - int(words_so_far)
 
-if days_until_start > 0:
-	days_remaining = project_length_days
-	print "Your timeline does not begin for another %s days.  We will use the full number of days between your start date and end date, %s, to calculate your needed words per day." % (days_until_start, project_length_days)
-	words_per_day = int(math.ceil(float(words_remaining) / project_length_days)) #Must cast at least one value as a float to get 'true' division in Python 2 (versus floor division)
-	print "Including your start and end dates, you will need to write at least %s words per day to meet your goal of %s words by the end of %s." % (words_per_day, words_total, end_date)
-else:
-	days_remaining_delta = __format_datetime(end_date_full) - __format_datetime(today_full)
-	days_remaining = days_remaining_delta.days
-	include_today = raw_input("Do you want to include today in your calculations? Answer with Y for yes or N for no. ")
-	if include_today == "Y":
-		days_remaining = days_remaining + 1
-		include_today_phrase = "including today"
+	if days_until_start > 0:
+		days_remaining = project_length_days
+		print "Your timeline does not begin for another %s days.  We will use the full number of days between your start date and end date, %s, to calculate your needed words per day." % (days_until_start, project_length_days)
+		words_per_day = int(math.ceil(float(words_remaining) / project_length_days)) #Must cast at least one value as a float to get 'true' division in Python 2 (versus floor division)
+		print "Including your start and end dates, you will need to write at least %s words per day to meet your goal of %s words by the end of %s." % (words_per_day, words_total, end_date)
 	else:
-		include_today_phrase = "not including today"
-	print "Today is %s, so you have %s days left before your deadline (%s)." % (today_date, days_remaining, include_today_phrase)
-	words_per_day = int(math.ceil(float(words_remaining) / days_remaining)) #Must cast at least one value as a float to get 'true' division in Python 2 (versus floor division)
-	print "Including your deadline day, you will need to write at least %s words per day to meet your goal (%s) of %s words by the end of %s." % (words_per_day, include_today_phrase, words_total, end_date)
-print "---------------------------------------------------------------"
-print "You have %s words and %s days remaining.  Good luck!" % (words_remaining, days_remaining)
-print "---------------------------------------------------------------"
+		days_remaining_delta = __format_datetime(end_date_full) - __format_datetime(today_full)
+		days_remaining = days_remaining_delta.days
+		include_today = raw_input("Do you want to include today in your calculations? Answer with Y for yes or N for no. ")
+		if include_today == "Y":
+			days_remaining = days_remaining + 1
+			include_today_phrase = "including today"
+		else:
+			include_today_phrase = "not including today"
+		print "Today is %s, so you have %s days left before your deadline (%s)." % (today_date, days_remaining, include_today_phrase)
+		words_per_day = int(math.ceil(float(words_remaining) / days_remaining)) #Must cast at least one value as a float to get 'true' division in Python 2 (versus floor division)
+		print "Including your deadline day, you will need to write at least %s words per day to meet your goal (%s) of %s words by the end of %s." % (words_per_day, include_today_phrase, words_total, end_date)
+	print "---------------------------------------------------------------"
+	print "You have %s words and %s days remaining.  Good luck!" % (words_remaining, days_remaining)
+	print "---------------------------------------------------------------"
+elif method == "B":
+	start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	while(len(start_date) == 0):
+		print "Sorry, no input was detected.  Please try again."
+		start_date = raw_input("What is the starting date for your writing project?  It can be any past, present, or future date.  (Please answer in the format mm/dd/yyyy, where the year is four digits.) ")
+	start_date_full = start_date + " 00:00:00"
+
+	goal_wpd = raw_input("How many words per day do you plan to write? ")
+	while(__check_numeric(goal_wpd) == 0):
+		print "Sorry, that did not appear to be a number.  Please try again."
+		goal_wpd = raw_input("How many words per day do you plan to write? ")
+
+	goal_total = raw_input("What is the total number of words you plan to write? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
+	while(__check_numeric(goal_total) == 0):
+		print "Sorry, that did not appear to be a number.  Please try again."
+		goal_total = raw_input("What is the total number of words you plan to write? Just enter the number, no commas (i.e. 50000 for fifty thousand). ")
+
+	words_so_far = raw_input("Have you written any words so far that should be subtracted from your total? Just enter the number, no commas.  (If you are not including any words already written, just enter 0). ")
+	while(__check_numeric(words_so_far) == False):
+		print "Sorry, that did not appear to be a number.  Please try again."
+		words_so_far = raw_input("Have you written any words so far that should be subtracted from your total? Just enter the number, no commas.  (If you are not including any words already written, just enter 0). ")
+
+	#Factor any already written words into the remaining total
+	#If the user enters 0, then remaining_words is just equal to goal_total
+	remaining_words = int(goal_total) - int(words_so_far)
+
+	#Calculate number of days needed to meet this goal
+	days_needed = int(math.ceil(float(remaining_words) / int(goal_wpd))) #Must cast at least one value as a float to get 'true' division in Python 2 (versus floor division)
+
+	#Find end date using relativedelta and convert to mm/dd/YYYY format
+	end_date = (__format_datetime(start_date_full) + relativedelta(days=+days_needed)).strftime("%m/%d/%Y")
+
+	print "---------------------------------------------------------------"
+	print "If you write %s words per day, including your start date, you will reach %s words on %s.  It will take %s days to reach your goal total." % (goal_wpd, goal_total, end_date, days_needed)
+	print "You have %s words remaining.  Good luck!" % (remaining_words)
+	print "---------------------------------------------------------------"
